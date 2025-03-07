607646f5b387a8c699d582f70ea1c34bbd055156
AnsonCar <anson1tsang@gmail.com>
2025-03-08 07:13:22 +0800

    feat: update models

--- utils/BaseModelMixin.py
+++ utils/BaseModelMixin.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class BaseModelMixin(models.Model):
     id = models.AutoField(primary_key=True)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
